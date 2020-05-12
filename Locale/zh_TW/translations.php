2954490ba5cc2fe87d9bbdb7b1454aff0f92f465
phil <phil@flexsystem.com>
2020-05-12 21:48:40 +0800

    + add zh_TW/translations.php

---
+++ Locale/zh_TW/translations.php
@@ -0,0 +1,12 @@
+<?php
+
+return array(
+    'Calendar' => '日程表',
+    'Calendar settings' => '日程設置',
+    'Project calendar view' => '項目日曆表',
+    'User calendar view' => '用戶日程視圖',
+    'My calendar' => '我的日程表',
+    'Switch to the list view' => '切換到列表視圖',
+    'Show tasks based on the start date' => '顯示任務開始日期於',
+    'Subtasks time tracking' => '子任務時間跟蹤',
+);
